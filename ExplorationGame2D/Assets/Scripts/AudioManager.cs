8bb8e8c5609c7e5918e31706f87e3c47bab0954c
OneZzzz <93658870+OneZzzz@users.noreply.github.com>
2022-03-05 13:08:55 +0800

    Revert "Revert "movement update""
    This reverts commit b2f183e85e646c2eb058862f53ca827f661248fa.

---
+++ ExplorationGame2D/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public AudioClip jump, run, gem, coin, openDoor, key;
+
+
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
